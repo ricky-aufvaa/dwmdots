e2720a69e25ee8c1f3938ae1110610d10feae6ba
ricky-aufvaa <aufvaa@gmail.com>
2022-07-22 13:53:03 +0530

    Dwm dots Fri 22 Jul 13:53:03 IST 2022

---
+++ .config/spotify/Users/b5ph7x683gad9tv6ho6xwjkd4-user/frecency.pb
@@ -0,0 +1,8 @@
+
+>
+'spotify:playlist:37i9dQZF1DWXmlLSKkfdAk	Ê†À%x@ Ïšÿ”
+>
+'spotify:playlist:2QT68Q0egPz7uYqcwkmNTL	QVÀ>¹x@ Ïí¨•
+>
+'spotify:playlist:37i9dQZF1EVKuMoAJjoTIw	Ç
+—o6”x@ ‹ùä–
--- collector.sh
+++ collector.sh
@@ -1,20 +1,19 @@
 
 
-sudo cp -r /home/riclin/dwm home/riclin/repos/dwmdot
-sudo cp -r /usr/local/bin home/riclin/repos/dwmdot
-sudo cp -r home/riclin/pix/wall home/riclin/repos/dwmdot
-sudo cp -r /etc/grub.d/40_custom home/riclin/repos/dwmdot
-sudo cp -r /home/riclin/.config home/riclin/repos/dwmdot
-sudo cp -r /home/riclin/.mozilla home/riclin/repos/dwmdot
-sudo cp -r /home/riclin/.cache/mozilla/firefox home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/dwm /home/riclin/repos/dwmdot
+sudo cp -r /usr/local/bin /home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/pix/wall /home/riclin/repos/dwmdot
+sudo cp  /etc/grub.d/40_custom /home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/.config /home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/.mozilla /home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/.cache/mozilla/firefox /home/riclin/repos/dwmdot
 rm -r .cache/mozilla/firefox/j0o2tf7c.default-release/cache2/entries
-echo "# dwmdots" >> README.md
+#echo "# dwmdots" >> README.md
 git init
-git add README.md
+#git add README.md
 git add .
 date=$(date)
 git commit -m "Dwm dots $date"
 git branch -M 'main'
 git remote add origin https://github.com/ricky-aufvaa/dwmdots.git
 git push -u origin 'main'
-
