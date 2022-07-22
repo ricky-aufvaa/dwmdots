139ba774f7b1d833e2a07b9bfdeb04ace0418492
ricky-aufvaa <aufvaa@gmail.com>
2022-07-22 13:39:21 +0530

    Dwm dots Fri 22 Jul 13:39:21 IST 2022

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # dwmdots
 # dwmdots
 # dwmdots
+# dwmdots
--- collector.sh
+++ collector.sh
@@ -1,10 +1,13 @@
 
 
-cp -r /home/riclin/dwm home/riclin/repos/dwmdot
-cp -r /usr/local/bin home/riclin/repos/dwmdot
-cp -r home/riclin/pix/wall home/riclin/repos/dwmdot
-cp -r /etc/grub.d/40_custom home/riclin/repos/dwmdot
-
+sudo cp -r /home/riclin/dwm home/riclin/repos/dwmdot
+sudo cp -r /usr/local/bin home/riclin/repos/dwmdot
+sudo cp -r home/riclin/pix/wall home/riclin/repos/dwmdot
+sudo cp -r /etc/grub.d/40_custom home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/.config home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/.mozilla home/riclin/repos/dwmdot
+sudo cp -r /home/riclin/.cache/mozilla/firefox home/riclin/repos/dwmdot
+rm -r .cache/mozilla/firefox/j0o2tf7c.default-release/cache2/entries
 echo "# dwmdots" >> README.md
 git init
 git add README.md
